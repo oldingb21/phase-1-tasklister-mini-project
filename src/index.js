305d5b9f3db8557637530b3cf8eb04c3a13e6872
Brenden Olding <oldingb21@gmail.com>
2023-05-04 22:07:55 -0400

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+const createTask = (task) => {
+  let li = document.createElement('li');
+  let btn = document.createElement('button')
+  btn.addEventListener('click', removeTask)
+  btn.textContent = 'X'
+  li.textContent = `${task} `
+  document.querySelector('#tasks').appendChild(li);
+  li.appendChild(btn);
+}
+
+const removeTask = (e) => {
+  e.target.parentNode.remove()
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    createTask(e.target.new_task_description.value)
+    form.reset()
+  })
 });
